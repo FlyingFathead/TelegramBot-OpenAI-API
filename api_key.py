77f729af60f86c9056ec078f21c2d047d559c988
FlyingFathead <flyingfathead@protonmail.com>
2023-12-28 20:26:44 +0200

    Updated bot_token.py and api_key.py for better error handling

--- api_key.py
+++ api_key.py
@@ -1,6 +1,6 @@
-# ~~~ read the openai api key ~~~
-
+# ~~~ read the OpenAI API key ~~~
 import os
+import sys
 
 def get_api_key():
     api_key = os.getenv('OPENAI_API_KEY')
@@ -9,5 +9,6 @@ def get_api_key():
             with open('api_token.txt', 'r') as file:
                 api_key = file.read().strip()
         except FileNotFoundError:
-            raise FileNotFoundError("The OPENAI_API_KEY environment variable is not set, and api_token.txt was not found.")
+            print("The OPENAI_API_KEY environment variable is not set, and `api_token.txt` was not found. Please configure either the `OPENAI_API_KEY` environment variable, OR insert your OpenAI API key inside a text file in this directory named `api_token.txt`.")
+            sys.exit(1)
     return api_key
--- bot_token.py
+++ bot_token.py
@@ -1,6 +1,6 @@
 # ~~~ read the telegram bot token ~~~
-
 import os
+import sys
 
 def get_bot_token():
     bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
@@ -9,5 +9,6 @@ def get_bot_token():
             with open('bot_token.txt', 'r') as file:
                 bot_token = file.read().strip()
         except FileNotFoundError:
-            raise FileNotFoundError("The TELEGRAM_BOT_TOKEN environment variable is not set, and bot_token.txt was not found.")
+            print("The TELEGRAM_BOT_TOKEN environment variable is not set, and `bot_token.txt` was not found. Please configure the either the `TELEGRAM_BOT_TOKEN` environment variable, OR insert your TG bot token inside a text file in this directory named `bot_token.txt`.")
+            sys.exit(1)
     return bot_token
