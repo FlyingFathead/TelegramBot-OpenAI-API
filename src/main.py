ce12a132123c9ea4bfe9051574d3e0a508b50439
FlyingFathead <flyingfathead@protonmail.com>
2025-04-04 14:17:51 +0300

    `v0.76` - reminders

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # version of this program
-version_number = "0.76"
+version_number = "0.761"
 
 # Add the project root directory to Python's path
 import sys
